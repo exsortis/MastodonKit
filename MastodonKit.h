d0cce5201b7529c00b235677b957a7e60c3d4392
Paul Schifferer <paul@schifferers.net>
2017-05-03 19:09:51 -0700

    Add Info.plist and umbrella header

---
+++ MastodonKit.h
@@ -0,0 +1,19 @@
+//
+//  MastodonKit.h
+//  MastodonKit
+//
+//  Created by Paul Schifferer on 25/4/17.
+//  Copyright © 2017 Pilgrimage Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MastodonKit.
+FOUNDATION_EXPORT double MastodonKitVersionNumber;
+
+//! Project version string for MastodonKit.
+FOUNDATION_EXPORT const unsigned char MastodonKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MastodonKit/PublicHeader.h>
+
+
